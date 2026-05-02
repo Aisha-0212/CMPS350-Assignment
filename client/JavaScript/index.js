87b2804e370212d4384d72e1f2b727695c5fef21
Mariam Abdullatif <mariam.emad2808@gmail.com>
2026-05-02 20:22:51 +0300

    fixed index.js

--- client/JavaScript/index.js
+++ client/JavaScript/index.js
@@ -1,9 +1,7 @@
-initAppData();
-
-const user = getCurrentUser();
-if(user){
-    window.location.href="feed.html"
-}
-else{
-    window.location.href = "login.html"
+const params = new URLSearchParams(window.location.search);
+const userId = params.get('userId');
+if (userId) {
+    window.location.href = `feed.html?userId=${userId}`;
+} else {
+    window.location.href = 'login.html';
 }
